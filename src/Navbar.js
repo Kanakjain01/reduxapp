7f84cf67a7c6b535ad4e2a42381f57f35d0e92c6
kanak <kanakjain639@gmail.com>
2023-07-05 02:16:20 +0530

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const amount = useSelector(state => state.amount)
+  const amount = useSelector((state) => state.amount);
   return (
-   
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
@@ -34,12 +33,12 @@ const Navbar = () => {
                   About
                 </a>
               </li>
-             
-             
             </ul>
-           <div>
-            <button disabled={true} className="btn-primary">Your Balance : {amount}</button>
-           </div>
+            <div>
+              <button disabled={true} className="btn-primary">
+                Your Balance : {amount}
+              </button>
+            </div>
           </div>
         </div>
       </nav>
